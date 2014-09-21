5737e3d65693c2be074b7800b4b0f75981234d04
Jon <jwood803@users.noreply.github.com>
2014-09-21 17:12:40 -0400

    Build all Example files

--- build.fsx
+++ build.fsx
@@ -7,16 +7,14 @@ open Fake.FscHelper
 
 let allDirs = 
     DirectoryInfo(__SOURCE_DIRECTORY__).GetDirectories "*"
-        |> Array.filter(fun i -> not(i.Name.Contains(".")) && i.Name <> "robot-name" && i.Name <> "gigasecond")
+        |> Array.filter(fun i -> not(i.Name.Contains(".")))
         |> Array.collect(fun d -> filesInDirMatching "Example.fs" d)
 
 let exampleFiles = allDirs |> Array.map(fun i -> i.FullName) |> Array.toList
 
-let first = [exampleFiles.Head]
-
 Target "Default" (fun _ ->
-    first |> Fsc(fun param -> { param with FscTarget = Library
-                                           Output = "Example.dll" })
+    exampleFiles |> Fsc(fun param -> { param with FscTarget = Library
+                                                  Output = "Example.dll" })
 )
 
 RunTargetOrDefault "Default"
