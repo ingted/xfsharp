330a9fb4d5cecfc26d0bc109c8a5fb6d81860b8e
Jon <jwood803@users.noreply.github.com>
2014-09-25 17:12:07 -0400

    Reference NUnit to try to build all files

--- build.fsx
+++ build.fsx
@@ -5,15 +5,18 @@ open Fake
 open Fake.FileSystemHelper
 open Fake.FscHelper
 
+let nunitRef = __SOURCE_DIRECTORY__ @@ @"packages\NUnit.Runners\tools\nunit.framework.dll"
+
 let allDirs = 
     DirectoryInfo(__SOURCE_DIRECTORY__).GetDirectories "*"
         |> Array.filter(fun i -> not(i.Name.Contains(".")))
-        |> Array.collect(fun d -> filesInDirMatching "Example.fs" d)
+        |> Array.collect(fun d -> filesInDirMatching "*.fs" d)
 
 let exampleFiles = allDirs |> Array.map(fun i -> i.FullName) |> Array.toList
 
 Target "Default" (fun _ ->
     exampleFiles |> Fsc(fun param -> { param with FscTarget = Library
+                                                  References = [nunitRef]
                                                   Output = "Example.dll" })
 )
 
