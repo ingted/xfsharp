d7632c860835dfb96005de216814d182eb0229fe
Jon <jwood803@users.noreply.github.com>
2014-10-13 07:10:29 -0500

    Start on pythagorean triplet exercise

---
+++ pythagorean-triplet/Example.fs
@@ -0,0 +1,11 @@
+﻿module PythagoreanTriplet
+
+type PythagoreanTriplet(a, b, c) =
+    member x.Sum() = a + b + c
+
+    member x.Product() = a * b * c
+
+    member x.IsPythagorean() = a ** 2.0 + b ** 2.0 = c ** 2.0
+
+    //member x.Where() =
+        
---
+++ pythagorean-triplet/PythagoreanTripletTest.fs
@@ -0,0 +1,23 @@
+﻿module PythagoreanTripletTest
+
+open PythagoreanTriplet
+open NUnit.Framework
+
+[<TestFixture>]
+type PythagoreanTripletTest() =
+
+    [<Test>]
+    member test.``Calculates the sum``() =
+        Assert.That(PythagoreanTriplet(3.0, 4.0, 5.0).Sum(), Is.EqualTo(12))
+
+    [<Test>]
+    member test.``Calculates the product``() =
+        Assert.That(PythagoreanTriplet(3.0, 4.0, 5.0).Product(), Is.EqualTo(60))
+
+    [<Test>]
+    member test.``Can recognize a valid pythagorean``() = 
+        Assert.IsTrue(PythagoreanTriplet(3.0, 4.0, 5.0).IsPythagorean())
+
+    [<Test>]
+    member test.``Can recognize a valid pythagorean 2``() = 
+        Assert.IsFalse(PythagoreanTriplet(5.0, 6.0, 7.0).IsPythagorean())
